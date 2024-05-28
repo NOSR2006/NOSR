341801b88e0ed730f30e623d231570459c3f7719
NOSR <3599795910@qq.com>
2024-05-28 20:55:56 +0800

    0.1.5

--- js/index.js
+++ js/index.js
@@ -1,46 +1,47 @@
 window.onload = function () {
-    let all = document.querySelector('.slider')
-    let pots = document.querySelectorAll('.pot')
-    let pics = document.querySelectorAll('.pic')
-    let left = document.getElementById('b_left')
-    let right = document.getElementById('b_right')
-    let index = 0
-    let time = 0
-    let pic = 0
-    let timer
-
     function setHeight() {
-        document.querySelector('.pic_list').style.height = document.querySelector('.pic').style.height = document.querySelector('.pic_item').offsetHeight + 'px'
-        document.querySelector('.top').style.height = document.querySelector('.milky').offsetHeight + 'px'
+        setTimeout(() => {
+            document.querySelector('.pic_list').style.height = document.querySelector('.pic').style.height = document.querySelector('.pic_item').offsetHeight + 'px'
+            document.querySelector('.top').style.height = document.querySelector('.milky').offsetHeight + 'px'
+        }, 100)
     }
     window.onresize = setHeight
+
+    let pic = 0
+    const pict1 = document.querySelector('.pic_txt1')
+    const pict2 = document.querySelector('.pic_txt2')
     $.ajax({
         url: "https://api.oioweb.cn/api/bing",
         type: "GET",
         success: function (data) {
+            pict1.innerHTML = data.result[index].title
+            pict2.innerHTML = data.result[index].copyright
+            localStorage.setItem('data', JSON.stringify(data))
             document.querySelectorAll('.pic_item').forEach(function (item) {
                 if (pic < data.result.length) {
                     item.src = data.result[pic].url
-                    setHeight()
                     pic++
+                    setHeight()
                 }
             })
         }
     })
 
-
-    let goIndex = function () {
-        for (i = 0; i < pics.length; i++) {
-            pics[i].className = 'pic'
-        }
-        for (j = 0; j < pots.length; j++) {
-            pots[j].className = 'pot'
-        }
+    let index = 0
+    const pots = document.querySelectorAll('.pot')
+    const pics = document.querySelectorAll('.pic')
+    const pict = JSON.parse(localStorage.getItem('data'))
+    function goIndex() {
+        pics.forEach(pic => pic.className = 'pic')
+        pots.forEach(pot => pot.className = 'pot')
         pics[index].className = 'pic live'
         pots[index].className = 'pot live'
         time = 0
+        pict1.innerHTML = pict.result[index].title
+        pict2.innerHTML = pict.result[index].copyright
     }
 
+    document.getElementById('pot_right').addEventListener('click', goRight)
     function goRight() {
         if (index == pics.length - 1) {
             index = 0
@@ -49,10 +50,7 @@ window.onload = function () {
         }
         goIndex()
     }
-
-    right.addEventListener('click', goRight)
-
-    left.addEventListener('click', function () {
+    document.getElementById('pot_left').addEventListener('click', () => {
         if (index == 0) {
             index = pics.length - 1
         } else {
@@ -61,29 +59,23 @@ window.onload = function () {
         goIndex()
     })
 
-    for (i = 0; i < pots.length; i++) {
-        pots[i].addEventListener('click', function () {
-            let potIndex = this.getAttribute('data-index')
-            index = potIndex
-            goIndex()
-        })
-    }
+    pots.forEach(pot => pot.addEventListener('click', (e) => {
+        const potIndex = e.target.getAttribute('data-index')
+        index = potIndex
+        goIndex()
+    }))
 
+    let time = 0
+    let timer
     function play() {
         timer = setInterval(() => {
-            time++
             if (time == 3) {
                 goRight()
+                time = 0
+            } else {
+                time++
             }
         }, 1000)
     }
     play()
-
-    all.onmousemove = function () {
-        clearInterval(timer)
-    }
-
-    all.onmouseleave = function () {
-        play()
-    }
 }
