9724302a7cef2912f6bff9dcfa8a7677de6a0c0f
NOSR <3599795910@qq.com>
2024-05-30 20:01:14 +0800

    0.2.1
    New!

--- js/index.js
+++ js/index.js
@@ -1,71 +1,63 @@
-window.onresize = setHeight
+$(window).on('resize', setHeight)
 function setHeight() {
     setTimeout(() => {
-        document.querySelector('.pic_list').style.height = document.querySelector('.pic').style.height = document.querySelector('.pic_item').offsetHeight + 'px'
-        document.querySelector('.top').style.height = document.querySelector('.milky').offsetHeight + 'px'
-    }, 100)
+        $('.pic_list, .pic').css('height', $('.pic_item').outerHeight())
+    }, 250)
 }
 
-let pic = 0
 $.ajax({
     url: "https://api.oioweb.cn/api/bing",
     type: "GET",
-    success: function (data) {
-        localStorage.setItem('data', JSON.stringify(data))
-        goIndex()
-        document.querySelectorAll('.pic_item').forEach(function (item) {
-            if (pic < data.result.length) {
-                item.src = data.result[pic].url
-                setHeight()
-                pic++
-            }
+    success: (data) => {
+        localStorage.setItem('data', JSON.stringify(data));
+        $('.pic_item').each(function (i) {
+            $(this).attr('src', data.result[i].url)
         })
+        setHeight()
+        goIndex()
     }
 })
 
 let index = 0
-const pots = document.querySelectorAll('.pot')
-const pics = document.querySelectorAll('.pic')
+const pots = $('.pot')
+const pics = $('.pic')
 const pict = JSON.parse(localStorage.getItem('data'))
 function goIndex() {
-    pics.forEach(pic => pic.className = 'pic')
-    pots.forEach(pot => pot.className = 'pot')
-    pics[index].className = 'pic live'
-    pots[index].className = 'pot live'
+    pics.removeClass('live')
+    pots.removeClass('live')
+    pics.eq(index).addClass('live')
+    pots.eq(index).addClass('live')
+    $('.pic_txt1').html(pict.result[index].title)
+    $('.pic_txt2').html(pict.result[index].copyright)
     time = 0
-    document.querySelector('.pic_txt1').innerHTML = pict.result[index].title
-    document.querySelector('.pic_txt2').innerHTML = pict.result[index].copyright
 }
 
-document.querySelector('#pot_right').addEventListener('click', goRight)
+$('#pot_right').on('click', goRight)
 function goRight() {
-    if (index == pics.length - 1) {
-        index = 0
-    } else {
-        index++
-    }
+    index = (index + 1) % pics.length
     goIndex()
 }
-document.querySelector('#pot_left').addEventListener('click', () => {
-    if (index == 0) {
-        index = pics.length - 1
-    } else {
-        index--
-    }
+
+$('#pot_left').on('click', () => {
+    index = (index - 1 + pics.length) % pics.length
     goIndex()
 })
 
-pots.forEach(pot => pot.addEventListener('click', (e) => {
-    const potIndex = e.target.getAttribute('data-index')
-    index = potIndex
+$('.pot').on('click', function () {
+    index = $(this).data('index')
     goIndex()
-}))
+})
+
+$('.pot').on('mouseenter', function () {
+    $(this).siblings('.pot').not(this).css('background-color', '#0d1117')
+}).on('mouseleave', function () {
+    $(this).siblings('.pot').css('background-color', '')
+})
 
 let time = 0
 setInterval(() => {
-    if (time == 3) {
+    if (time === 3) {
         goRight()
-        time = 0
     } else {
         time++
     }
@@ -75,13 +67,13 @@ navigator.geolocation.getCurrentPosition((i) => {
     $.ajax({
         url: `https://api.oioweb.cn/api/ip/geocoder?lng=${i.coords.longitude}&lat=${i.coords.latitude}`,
         type: "GET",
-        success: function (loc) {
-            $('#wea0').text('城市：' + loc.result.ad_info.city)
+        success: (loc) => {
+            $('#wea1').text('城市：' + loc.result.ad_info.city)
             $.ajax({
                 url: `https://api.oioweb.cn/api/weather/weather?city_name=${loc.result.ad_info.city}`,
                 type: "GET",
-                success: function (data) {
-                    $('#wea1').text('日 期：' + data.result.forecast_list[1].date)
+                success: (data) => {
+                    $('.top_right label').text('提 醒：' + data.result.tips)
                     $('#wea2').text('天 气：' + data.result.hourly_forecast[1].condition)
                     $('#wea3').text('空 气：' + data.result.quality_level)
                     $('#wea4').text('风 向：' + data.result.wind_direction)
